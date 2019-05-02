f53217d970260125a9fe36e456f5d4e53dab53ab
ivanzk <28441880+ivanzk@users.noreply.github.com>
2019-05-02 18:26:51 +0200

    Add iframe style

--- src/open-in-iframe/index.js
+++ src/open-in-iframe/index.js
@@ -1,6 +1,10 @@
 export default (url, callbackFn) => {
   const iframe = document.createElement('iframe');
   iframe.src = url;
+  iframe.style = `
+    height: 0px;
+    opacity: 0;
+  `;
   document.body.append(iframe);
 
   return new Promise(resolve => {
