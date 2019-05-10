a28a25e90ddeee392bbdbb1bb8f5acbf0e6080a2
ivanzk <28441880+ivanzk@users.noreply.github.com>
2019-05-10 21:32:33 +0200

    Add throttle

--- src/throttle/index.js
+++ src/throttle/index.js
@@ -2,8 +2,6 @@ export default (delay, callbackFn) => {
   let lastCall = 0;
 
   return function(...args) {
-    console.log('args: ', args);
-    console.log('lastCall: ', lastCall);
     const now = new Date().getTime();
 
     if (now - lastCall < delay) {
