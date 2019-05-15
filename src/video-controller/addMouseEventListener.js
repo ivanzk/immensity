d6e649f5d48dbd5fcbd636ad36ab54e2a62c2651
ivanzk <28441880+ivanzk@users.noreply.github.com>
2019-05-16 01:52:30 +0200

    Add more controls

--- src/video-controller/addMouseEventListener.js
+++ src/video-controller/addMouseEventListener.js
@@ -15,6 +15,11 @@ export default (videoController, opt) => {
     // Left
     'wheeldown+g0208': videoController.decreasePlaybackRate,
     'wheelup+g0208': videoController.increasePlaybackRate,
+    'alt+wheeldown+g0208': () => videoController.decreasePlaybackRate(0.05),
+    'alt+wheelup+g0208': () => videoController.increasePlaybackRate(0.05),
+    'shift+alt+wheeldown+g0208': () =>
+      videoController.decreasePlaybackRate(0.5),
+    'shift+alt+wheelup+g0208': () => videoController.increasePlaybackRate(0.5),
     'mousedown0+g0228': videoController.normalizePlaybackRate,
     'mousedown0+g0201': () => getVideoshot(video, opt.videoshotOpt),
 
@@ -38,12 +43,22 @@ export default (videoController, opt) => {
 
     'wheeldown+g3658': () => videoController.seekBackward(2.5),
     'wheelup+g3658': () => videoController.seekForward(2.5),
+    'alt+wheeldown+g3658': () => videoController.seekBackward(0.1),
+    'alt+wheelup+g3658': () => videoController.seekForward(0.1),
+    'shift+alt+wheeldown+g3658': () =>
+      videoController.seekBackward(video.duration / 10),
+    'shift+alt+wheelup+g3658': () =>
+      videoController.seekForward(video.duration / 10),
 
     // Right
     'wheeldown+g7904': () => videoController.volumeDown(0.05),
     'wheelup+g7904': () => videoController.volumeUp(0.05),
     'wheeldown+g7958': () => videoController.volumeDown(0.01),
     'wheelup+g7958': () => videoController.volumeUp(0.01),
+    'alt+wheeldown+g7904': () => videoController.volumeDown(0.05 * 10),
+    'alt+wheelup+g7904': () => videoController.volumeUp(0.05 * 10),
+    'alt+wheeldown+g7958': () => videoController.volumeDown(0.01 * 10),
+    'alt+wheelup+g7958': () => videoController.volumeUp(0.01 * 10),
     'mousedown0+g7928': videoController.toggleMute
   };
 
